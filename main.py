33ede2964af369d82bf36f39f42a230a883cd955
trapala85mx <rtrapala.dev.ds@gmail.com>
2025-02-10 18:26:58 -0600

    First commit. Agrega gitignore, algunas constantes en paquete utils y paquete api

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from bingx-api!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/const.py
@@ -0,0 +1,54 @@
+from enum import StrEnum
+
+
+class Endpoints(StrEnum):
+    """Constantes para las urls / endpoints de Bingx"""
+
+    BASE_URL = "https://open-api.bingx.com"
+    ACCOUNT = "/openApi/swap/v3/user/balance"
+    CONTRACTS = "/openApi/swap/v2/quote/contracts"
+    KLINES = "/openApi/swap/v3/quote/klines"
+    PLACE_ORDER = "/openApi/swap/v2/trade/order"
+    QUERY_MARGIN_TYPE = "/openApi/swap/v2/trade/marginType"
+
+
+class Intervals(StrEnum):
+    """Constantes para los timeframes"""
+
+    KLINE_1_MIN = "1m"
+    KLINE_5_MIN = "5m"
+    KLINE_15_MIN = "15m"
+    KLINE_4_HOUR = "4h"
+    KLINE_1_DAY = "1d"
+    KLINE_1_MONT = "1M"
+    KLINE_1_WEEK = "1w"
+
+
+class OrderType(StrEnum):
+    """Constantes para tipo de órdenes"""
+
+    LIMIT_ORDER = "LIMIT"
+    MARKET_ORDER = "MARKET"
+    STOP_MARKET_ORDER = "STOP_MARKET"
+    TAKE_PROFIT_MARKET_ORDER = "TAKE_PROFIT_MARKET"
+    STOP_LIMIT_ORDER = "STOP"
+    TAKE_PROFIT_LIMIT_ORDER = "TAKE_PROFIT"
+    STOP_LIMIT_ORDER_WITH_TRIGGER = "TRIGGER_LIMIT"
+    STOP_MARKET_ORDER_WITH_TRIGGER = "TRIGGER_MARKET"
+    TRAILING_STOP_MARKET_ORDER = "TRAILING_STOP_MARKET"
+    TRAILING_TAKEPROFIT_OR_STOPLOSS = "TRAILING_TP_SL"
+
+
+class PositionSide(StrEnum):
+    """Constantes para tipo de posición"""
+
+    BOTH = "BOTH"
+    LONG = "LONG"
+    SHORT = "SHORT"
+
+
+class MarginType(StrEnum):
+    """Tipo de marge"""
+
+    ISOLATED = "ISOLATED"
+    CROSSED = "CROSSED"
